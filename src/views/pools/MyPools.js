f04861b204b9b4e588d0893c5cfe04bbfae4d6b5
AndreaScapellato <andrea.scapellato94@gmail.com>
2020-10-26 16:38:10 +0100

    prova con dati fissi

--- src/views/pools/MyPools.js
+++ src/views/pools/MyPools.js
@@ -1,35 +1,66 @@
-import React, {Component} from 'react';
-import PoolCard from './PoolCard';
-import ContentWrapper from '../../components/layout/ContentWrapper';
-import TokenManager from '../../components/auth/Token';
+import React, { Component } from "react";
+import PoolCard from "./PoolCard";
+import ContentWrapper from "../../components/layout/ContentWrapper";
+import TokenManager from "../../components/auth/Token";
 
 class MyPools extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      description: "",
+      quote: "",
+      stake: "",
+      profit: "",
+      bookmaker: "",
+      pools: [
+        {
+          id: 1,
+          description: "descrizione",
+          totalQuote: 50,
+          stake: 10,
+          profit: 1000,
+          bookmaker: "William Hill",
+          totalEvents: [],
+          poolCreatedOn: "data",
+          poolUpdatedOn: "2",
+          hrefPool: "link",
+        },
+      ],
+    };
+    //this.getMyPools();
+  }
 
-    constructor(props, context) {
-        super(props, context);
-        this.state = {
-            description: '',
-            quote: '',
-            stake: '',
-            profit: '',
-            bookmaker: '',
-            pools: [],
-        };
-        this.getMyPools();
-    }
+  async getMyPools() {
+    var token = await TokenManager.getInstance().getToken();
+    fetch("http://localhost:5005/pools", {
+      method: "GET",
+      headers: { "Content-Type": "application/json", "X-Auth": token },
+    })
+      .then((response) => response.json())
+      .then((response) => this.setState({ pools: response._embedded.pools }));
+  }
 
-    async getMyPools() {
-        var token =  await TokenManager.getInstance().getToken();
-        fetch('http://localhost:5005/pools', {method: 'GET', headers: {'Content-Type': 'application/json', 'X-Auth': token}}).then((response) => response.json()).then((response) => this.setState({pools: response._embedded.pools}));
-    }
-
-    render() {
-        return (
-            <ContentWrapper>
-                {this.state.pools.map((pool) => <PoolCard key={pool._links.self.href} id={pool.id} description={pool.description} totalQuote={pool.totalQuote} stake={pool.stake} profit={pool.profit} bookmaker={pool.bookmaker} totalEvents={pool.totalEvents} poolCreatedOn={pool.createdOn} poolUpdatedOn={pool.updatedOn} hrefPool={pool._links.self.href}></PoolCard>)}
-            </ContentWrapper>
-        )
-    }
+  render() {
+    return (
+      <ContentWrapper>
+        {this.state.pools.map((pool) => (
+          <PoolCard
+            key={1}
+            id={pool.id}
+            description={pool.description}
+            totalQuote={pool.totalQuote}
+            stake={pool.stake}
+            profit={pool.profit}
+            bookmaker={pool.bookmaker}
+            totalEvents={pool.totalEvents}
+            poolCreatedOn={pool.createdOn}
+            poolUpdatedOn={pool.updatedOn}
+            hrefPool={"pool._links.self.href"}
+          ></PoolCard>
+        ))}
+      </ContentWrapper>
+    );
+  }
 }
 
-export default MyPools
+export default MyPools;
