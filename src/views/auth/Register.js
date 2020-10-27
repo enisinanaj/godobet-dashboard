9e759e5fae4ffa51af36e4bde935f686fc376fca
Eni Sinanaj <enisinanaj@outlook.com>
2020-10-27 23:01:33 +0100

    carcamento utente da db locale al login

--- src/views/auth/Register.js
+++ src/views/auth/Register.js
@@ -69,9 +69,7 @@ class Register extends Component {
         .then(e => {
             e.user.sendEmailVerification();
             user.name = e.user.displayName;
-            return e.user.getIdToken();
-        })
-        .then(accessToken => {
+            
             return TokenManager.getInstance().getToken()
             .then(jwt => {
                 return fetch("http://localhost:5005/users", {
@@ -83,7 +81,7 @@ class Register extends Component {
                     body: JSON.stringify({
                         ...user,
                         password: "",
-                        accessToken
+                        accessToken: e.user.uid
                     })
                 })
             })
