d6c1aafa3fcfd8f12e68411351040a663db32032
Ani Sinanaj <sinanajani@outlook.com>
2021-02-20 11:34:51 +0100

    Fixed user links in top bar user menu

--- src/App/layout/AdminLayout/NavBar/NavRight/index.js
+++ src/App/layout/AdminLayout/NavBar/NavRight/index.js
@@ -36,8 +36,8 @@ class NavRight extends Component {
                                     </a>
                                 </div>
                                 <ul className="pro-body">
-                                    <li><a href={DEMO.BLANK_LINK} className="dropdown-item"><i className="feather icon-settings"/> All Users</a></li>
-                                    <li><a href={DEMO.BLANK_LINK} className="dropdown-item"><i className="feather icon-user"/> Profile</a></li>
+                                    <li><a href="/users" className="dropdown-item"><i className="feather icon-settings"/> All Users</a></li>
+                                    <li><a href="/profile" className="dropdown-item"><i className="feather icon-user"/> Profile</a></li>
                                 </ul>
                             </Dropdown.Menu>
                         </Dropdown>
