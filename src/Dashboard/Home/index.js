a1b2111ca93c8cc5a9378d525023c5f81ff59515
Adrian Fabjanski <adrianfab19@gmail.com>
2021-04-12 12:16:18 +0200

    absolute button fix

--- src/Dashboard/Home/index.js
+++ src/Dashboard/Home/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Row, Col, Card } from 'react-bootstrap';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import moment from 'moment';
-import Chart from 'react-apexcharts';
-import Aux from '../../hoc/_Aux';
-import * as actions from '../../store/actions';
-import config from '../../store/config';
-import TokenManager from '../../App/auth/TokenManager';
-import satisfactionChart from './charts/pie';
-import LocaleNumber from '../../App/components/LocaleNumber';
+import React from "react";
+import { Row, Col, Card } from "react-bootstrap";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import moment from "moment";
+import Chart from "react-apexcharts";
+import Aux from "../../hoc/_Aux";
+import * as actions from "../../store/actions";
+import config from "../../store/config";
+import TokenManager from "../../App/auth/TokenManager";
+import satisfactionChart from "./charts/pie";
+import LocaleNumber from "../../App/components/LocaleNumber";
 
 class Default extends React.Component {
   state = {
@@ -20,35 +20,39 @@ class Default extends React.Component {
     totalProfit: 0,
     monthProfit: 0,
     subscribedPools: [],
-    startDate: moment().add(-3, 'month').toDate(),
-    endDate: moment().add(-1, 'day').toDate(),
+    startDate: moment().add(-3, "month").toDate(),
+    endDate: moment().add(-1, "day").toDate(),
     flotData: [],
   };
 
   getMyPools = () =>
     this.load(
       `${config.API_URL}/pools/search/subscriberPools?subscriber=${this.props.user._links.self.href}&page=0&size=1000`
-    ).then((p) => p._embedded.pools)
+    ).then((p) => p._embedded.pools);
 
   getStatsForMonth = () =>
     this.load(
       `${config.API_URL}/pools/search/subscriberStats?start=${moment(
         this.state.startDate
-      ).format('YYYY-MM-DDTHH:mm:ss.SSS')}&end=${moment(
+      ).format("YYYY-MM-DDTHH:mm:ss.SSS")}&end=${moment(
         this.state.endDate
-      ).format('YYYY-MM-DDTHH:mm:ss.SSS')}&subscriber=${
+      ).format("YYYY-MM-DDTHH:mm:ss.SSS")}&subscriber=${
         this.props.user._links.self.href
       }`
-    ).then((stats) => {console.warn(stats?._embedded?.pools); return stats?._embedded?.pools});
+    ).then((stats) => {
+      console.warn(stats?._embedded?.pools);
+      return stats?._embedded?.pools;
+    });
 
   getUserSubscriptions = () =>
     this.load(
       `${config.API_URL}/users/${this.props.user.userCode}/subscriptions?page=0&size=1000`
     ).then((subs) => subs?._embedded?.subscriptions);
 
-  getTipsterOpenPools = () => 
-    this.load(`${config.API_URL}/users/${this.props.user.userCode}/pools?page=0&size=1000`)
-    .then((openPools) => openPools?._embedded?.pools);
+  getTipsterOpenPools = () =>
+    this.load(
+      `${config.API_URL}/users/${this.props.user.userCode}/pools?page=0&size=1000`
+    ).then((openPools) => openPools?._embedded?.pools);
 
   getTipsterTotalSubscribers = () =>
     this.load(`${config.API_URL}/users/${this.props.user.userCode}`).then(
@@ -61,8 +65,8 @@ class Default extends React.Component {
       .then((jwt) =>
         fetch(url, {
           headers: {
-            'Content-Type': 'application/json',
-            'X-Auth': jwt,
+            "Content-Type": "application/json",
+            "X-Auth": jwt,
           },
           ...args,
         })
@@ -82,25 +86,41 @@ class Default extends React.Component {
       this.getStatsForMonth(),
       this.getTipsterOpenPools(),
       this.getTipsterTotalSubscribers(),
-    ]).then((p) => {
-      this.setState({
-        totalProfit: this.props.user.totalProfit,
-        pendingTipsCount: p[0].filter(p => !p.outcome).length,
-        subscribedPools: [...activeTipsCount, ...p[0]],
-        activeTipsCount: p[1]?.filter((s) => !s.expired && s.captured === 1)?.length,
-        monthProfit: p[2]?.reduce((a, b) => a + b.profit, 0),
-        openPools: p[3].filter(p => !p.outcome).length,
-        totalSubscribers: p[4],
-      });
+    ])
+      .then((p) => {
+        this.setState({
+          totalProfit: this.props.user.totalProfit,
+          pendingTipsCount: p[0].filter((p) => !p.outcome).length,
+          subscribedPools: [...activeTipsCount, ...p[0]],
+          activeTipsCount: p[1]?.filter((s) => !s.expired && s.captured === 1)
+            ?.length,
+          monthProfit: p[2]?.reduce((a, b) => a + b.profit, 0),
+          openPools: p[3].filter((p) => !p.outcome).length,
+          totalSubscribers: p[4],
+        });
 
-      return p[0];
-    })
-    .then(pendingTips => {
-      return this.load(`${this.props.user._links.self.href}/playedPoolsRel?page=0&size=1000`)
-      .then(playedPools => [pendingTips.filter(p => !p.outcome), playedPools._embedded.playedPools])
-    })
-    .then(poolsSets => poolsSets[0].filter(pool => !poolsSets[1].find(pp => pp.references.pool === pool.id && pp.direction !== FOLLOWED)))
-    .then(pendingTips => this.setState({pendingTipsCount: pendingTips.length}))
+        return p[0];
+      })
+      .then((pendingTips) => {
+        return this.load(
+          `${this.props.user._links.self.href}/playedPoolsRel?page=0&size=1000`
+        ).then((playedPools) => [
+          pendingTips.filter((p) => !p.outcome),
+          playedPools._embedded.playedPools,
+        ]);
+      })
+      .then((poolsSets) =>
+        poolsSets[0].filter(
+          (pool) =>
+            !poolsSets[1].find(
+              (pp) =>
+                pp.references.pool === pool.id && pp.direction !== FOLLOWED
+            )
+        )
+      )
+      .then((pendingTips) =>
+        this.setState({ pendingTipsCount: pendingTips.length })
+      );
   }
 
   componentDidMount() {
@@ -142,11 +162,22 @@ class Default extends React.Component {
 
     return (
       <Aux>
-        <Row style={{background: "#ecf0f5", padding: "20px", borderRadius: "5px"}}>
+        <Row
+          style={{
+            background: "#ecf0f5",
+            padding: "20px",
+            borderRadius: "5px",
+          }}
+        >
           <Col xl={12} md={12}>
             <Row md={12}>
-              <Col md={12} lg={12} >
-                <h4>Iscrizioni <small style={{fontSize: "12px"}}>(Statistiche sui servizi a cui sei iscritto)</small></h4>
+              <Col md={12} lg={12}>
+                <h4>
+                  Iscrizioni{" "}
+                  <small style={{ fontSize: "12px" }}>
+                    (Statistiche sui servizi a cui sei iscritto)
+                  </small>
+                </h4>
               </Col>
               <Col md={6}>
                 <Card md={12}>
@@ -191,7 +222,10 @@ class Default extends React.Component {
                       <Col sm={8}>
                         <h6 className="text-muted m-b-0">Profitto Totale</h6>
                         <h4 className="text-c-yellow">
-                          <LocaleNumber amount={this.state.totalProfit} symbol={"%"} />
+                          <LocaleNumber
+                            amount={this.state.totalProfit}
+                            symbol={"%"}
+                          />
                         </h4>
                       </Col>
                       <Col sm={4} className="text-right">
@@ -208,7 +242,10 @@ class Default extends React.Component {
                       <Col sm={8}>
                         <h6 className="text-muted m-b-0">Profitto mensile</h6>
                         <h4 className="text-c-yellow">
-                          <LocaleNumber amount={this.state.monthProfit} symbol={"%"} />
+                          <LocaleNumber
+                            amount={this.state.monthProfit}
+                            symbol={"%"}
+                          />
                         </h4>
                       </Col>
                       <Col sm={4} className="text-right">
@@ -221,15 +258,22 @@ class Default extends React.Component {
             </Row>
             {this.props.user.roleValue >= 5 && (
               <Row md={12}>
-                <Col md={12} lg={12} >
-                  <h4>Gestione <small style={{fontSize: "12px"}}>(Panoramica dei servizi che hai creato e gestisci)</small></h4>
+                <Col md={12} lg={12}>
+                  <h4>
+                    Gestione{" "}
+                    <small style={{ fontSize: "12px" }}>
+                      (Panoramica dei servizi che hai creato e gestisci)
+                    </small>
+                  </h4>
                 </Col>
                 <Col md={6}>
                   <Card md={12}>
                     <Card.Body>
                       <Row className="align-items-center">
                         <Col sm={8}>
-                          <h6 className="text-muted m-b-0">Tips da refertare</h6>
+                          <h6 className="text-muted m-b-0">
+                            Tips da refertare
+                          </h6>
                           <h4 className="text-c-yellow">
                             {this.state.openPools}
                           </h4>
@@ -248,7 +292,9 @@ class Default extends React.Component {
                         <Col sm={8}>
                           <h6 className="text-muted m-b-0">Iscrizioni</h6>
                           <h4 className="text-c-yellow">
-                            {this.state.totalSubscribers === -1 ? 0 : this.state.totalSubscribers }
+                            {this.state.totalSubscribers === -1
+                              ? 0
+                              : this.state.totalSubscribers}
                           </h4>
                         </Col>
                         <Col sm={4} className="text-right">
@@ -261,8 +307,13 @@ class Default extends React.Component {
               </Row>
             )}
             <Row>
-              <Col md={12} lg={12} >
-                <h4>Tip seguite <small style={{fontSize: "12px"}}>(Distribuzione dell'esito delle tip che hai seguito)</small></h4>
+              <Col md={12} lg={12}>
+                <h4>
+                  Tip seguite{" "}
+                  <small style={{ fontSize: "12px" }}>
+                    (Distribuzione dell'esito delle tip che hai seguito)
+                  </small>
+                </h4>
               </Col>
               <Col>
                 <Card md={12}>
